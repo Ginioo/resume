4fb74bfcf4e140f35b917d0f0ff3e5e75ea18ebe
Gino Wu <nikeorz+github@gmail.com>
2016-09-02 07:24:54 +0000

    init

---
+++ app/index.js
@@ -0,0 +1,2 @@
+var component = require('./component');
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const parts = require('./libs/parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(
+      common,
+      parts.setupCSS(PATHS.app)
+    );
+    break;
+  default:
+    config = merge(
+      common,
+      parts.setupCSS(PATHS.app),
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.IP,
+        port: process.env.PORT
+      })
+    );
+}
+
+module.exports = validate(config);
+
