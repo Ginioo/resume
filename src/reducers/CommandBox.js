c402e771225b39aabe623f962bf70104a8a99101
Gino Wu <nikeorz+github@gmail.com>
2016-09-05 02:16:48 +0000

    fix warning

--- src/reducers/CommandBox.js
+++ src/reducers/CommandBox.js
@@ -8,10 +8,19 @@ export default function(state = initState, action) {
   switch(action.type) {
     case EXECUTE_COMMAND:
       const display = (action.payload.toLowerCase() == 'profile') ? 'block' : 'none';
+
+      if (state.commands.indexOf(action.payload.toLowerCase()) != -1) {
+        return {
+          commands: [...state.commands, action.payload],
+          show: display
+        };
+      }
+
       return {
-        commands: [...state.commands, action.payload],
+        commands: state.commands,
         show: display
       };
+
     default:
       return state;
   }
