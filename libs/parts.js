55ad0c105c60f8086c0672b8454f7e6197602da9
Gino Wu <nikeorz+github@gmail.com>
2016-09-14 12:02:40 +0000

    // This is used for require.ensure. The setup

--- libs/parts.js
+++ libs/parts.js
@@ -82,7 +82,7 @@ exports.minify = function() {
       })
     ]
   };
-};
+}
 
 exports.setupBabel = function() {
   return {
@@ -96,4 +96,21 @@ exports.setupBabel = function() {
       ]
     }
   };
-};
+}
+
+exports.extractBundle = function(options) {
+  const entry = {};
+  entry[options.name] = options.entries;
+
+  return {
+    // Define an entry point needed for splitting.
+    entry: entry,
+    plugins: [
+      // Extract bundle and manifest files. Manifest is
+      // needed for reliable caching.
+      new webpack.optimize.CommonsChunkPlugin({
+        names: [options.name, 'manifest']
+      })
+    ]
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const merge = require('webpack-merge');
 const validate = require('webpack-validator');
 const parts = require('./libs/parts');
 
+const pkg = require('./package.json');
+
 const PATHS = {
   app: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
@@ -14,12 +16,15 @@ const common = {
   // We'll be using the latter form given it's
   // convenient with more complex configurations.
   entry: {
-    vendor: ['react'],
-    app: PATHS.app
+    app: PATHS.app,
+    vendor: pkg.dependencies ? Object.keys(pkg.dependencies) : []
   },
   output: {
     path: PATHS.build,
-    filename: '[name].js'
+    filename: '[name].[chunkhash]js',
+    // This is used for require.ensure. The setup
+    // will work without but the is useful to set.
+    chunkFilename: '[chunkhash].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -44,6 +49,10 @@ switch(process.env.npm_lifecycle_event) {
       {
         devtool: 'source-map'
       },
+      parts.extractBundle({
+        name: 'vendor',
+        entries: []
+      }),
       parts.minify(),
       parts.setupCSS(PATHS.app),
       parts.setupBabel()
