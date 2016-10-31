f351d8a57ab1733ac0d3cdfd96d3923acc2b76b8
Gino <nikeorz@gmail.com>
2016-10-30 22:36:13 -0400

    update src/components/app

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,35 @@ export default class App extends Component {
                         title="Web Developer"
                         src="https://avatars0.githubusercontent.com/u/9994905?v=3&s=460"
                     />
+                    <div>
+                        /**
+                         * 1-10 Scale
+                         * 10 means the most familiar with
+                         */
+                        [tech stack : scale]
+                        <ul>
+                            <li> php 5.x : 6</li>
+                            <li> laravel 5.x  : 6</li>
+                            <li> phalcon : 3</li>
+                            <li> composer : 6</li>
+                            <li> git : 6</li>
+                            <li> docker : 5</li>
+                            <li> mysql : 5</li>
+                            <li> mssql : 6</li>
+                            <li> javascript（es6) : 6</li>
+                            <li> react.js : 5</li>
+                            <li> gulp : 4</li>
+                            <li> weback : 5</li>
+                            <li> angularJS 1.x : 4</li>
+                            <li> google app script : 3</li>
+                            <li> css : 3</li>
+                            <li> travis-ci : 3</li>
+                            <li> phpunit : 5</li>
+                            <li> behat : 5</li>
+                            <li> mocha : 5</li>
+                            <li> nightwatchjs : 6</li>
+                        </ul>
+                        </div>
                 </div>
             </div>
         );
