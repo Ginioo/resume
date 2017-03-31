90cb7ca373efe06525b56d626334fb5b34ae20a1
Gino <nikeorz@gmail.com>
2017-04-01 07:53:47 +0800

    update infromation for mixerbox

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -23,12 +23,12 @@ export default class Intro extends Component {
                 mainly responsible for developing backend service for mobile apps or the other systems while
                 preventing it from broken down since the system has been taken over by too many developers.
               </p>
-            </div>
-            <div className="col-lg-4">
               <p>Moreover, part of my role was responsible for learning new tech stacks such as git for version control,
                 nightwatchjs for test-automation, and then to introduce them to my colleagues. We went through each of
                 our projects using kanban board, which is a way to run a project within a team or between teams.
               </p>
+            </div>
+            <div className="col-lg-4">
               <p>
                 From 2 May 2017 to 30 Sep 2017, I worked for the <a href="https://www.bloomon.nl">Bloomon</a> platform
                 as a full-stack developer for few months. We built up this platform using React and angular 1.x for
