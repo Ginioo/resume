c2b86f83ffe01981f2a35c4a920c55dd570f8d32
Gino <nikeorz@gmail.com>
2019-02-06 23:07:10 +0800

    build ~2019.2.6

---
+++ build/precache-manifest.2b9bf7678764efd5000cc1c638e9705a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eefaf7b81bb83df3c793",
+    "url": "/static/js/main.eefaf7b8.chunk.js"
+  },
+  {
+    "revision": "5ce9b52ca37928e3a660",
+    "url": "/static/js/1.5ce9b52c.chunk.js"
+  },
+  {
+    "revision": "eefaf7b81bb83df3c793",
+    "url": "/static/css/main.b1b59e1e.chunk.css"
+  },
+  {
+    "revision": "efffad7a724c8e58ab744b1f38b0c298",
+    "url": "/index.html"
+  }
+];
